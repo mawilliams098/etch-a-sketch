55815508a652ec7ce21380801857b712099e0d6b
mawilliams098 <mawilliams098@gmail.com>
2024-03-17 16:18:03 -0400

    Create 16x16 grid of pixels

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('here');
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const pixel = document.createElement("div");
+        pixel.setAttribute("class", "pixel");
+        container.appendChild(pixel);
+    }
+}
